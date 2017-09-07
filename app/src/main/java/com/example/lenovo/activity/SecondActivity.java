d8c9d620a8dd38bf51a8986f20f274d0be360b01
hym <1349456160@qq.com>
2017-09-07 14:01:17 +0800

    活动间数据单向传送完成

--- app/src/main/java/com/example/lenovo/activity/SecondActivity.java
+++ app/src/main/java/com/example/lenovo/activity/SecondActivity.java
@@ -3,18 +3,40 @@ package com.example.lenovo.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    String te,s;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
         Intent intent=getIntent();
-        String te= intent.getStringExtra("editText");
-        
+        te= intent.getStringExtra("editText");
+        editText.setText("接收到的数据为"+te);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+         editText= (EditText) findViewById(R.id.edit);
+         s=editText.getText().toString();
+        switch(view.getId()){
+            case R.id.button:
+                Intent intent=new Intent();
+                intent.putExtra("return_data",s);
+                setResult(RESULT_OK,intent);
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
